4c1f723a2dc8171ffff8ecca57469773072801a6
Richard Melchior <raustin.melchior@gmail.com>
2018-06-02 18:01:07 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,5 +1,6 @@
 ##
-# #parrot takes an argument as a string and returns and outputs it
+# #parrot takes an argument as a string and returns and outputs it, if no argument, it outputs
+# and returns "Squawk!"
 ##
 def parrot(string = "Squawk!")
   puts string;
