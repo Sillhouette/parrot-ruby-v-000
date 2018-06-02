933939df3a4cd0f03ac0f207e67162b3b69b7375
Richard Melchior <raustin.melchior@gmail.com>
2018-06-02 18:00:30 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 ##
 # #parrot takes an argument as a string and returns and outputs it
 ##
-def parrot(string)
-  puts string
-  return string
+def parrot(string = "Squawk!")
+  puts string;
+  return string;
 end
